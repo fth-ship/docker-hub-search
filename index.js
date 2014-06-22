63c4e388a14ed6423af111964f8318d418307418
Kaique da Silva <kaique.developer@gmail.com>
2014-06-22 14:16:55 -0300

    simple version was complete

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 var app = require('commander');
+var chalk = require('chalk');
 var dhs = require('./lib');
 var pkg = require('./package');
+var sep = ' ' + ((new Array(47)).join('-'));
 
 app
   .version(pkg.version)
   .option('-s, --search [keyword]', 'Search in docker registry an image.')
   .parse(process.argv);
 
+function banner() {
+  console.log([
+    ' ==============================================', '\n',
+    '           Docker registry search', '\n',
+    ' ==============================================', '\n',
+  ].join(''));
+}
+
 function searchHandler(err, result) {
-  console.log('total of %s', result.length);
-  result.map(console.log.bind(console));
+  banner();
+
+  console.log(' Total of %s founded!', result.length);
+  console.log(sep);
+  console.log();
+
+  function resultMapHandler(item) {
+    console.log(' [ %s ] - %s\n', chalk.green(item.href), item.title);
+  }
+  result.map(resultMapHandler);
 }
 
 if (app.search) {
--- test/lib.test.js
+++ test/lib.test.js
@@ -2,10 +2,6 @@ var dhs = require('../lib');
 var should = require('should');
 
 describe('Dhs', function() {
-  it('should be clean title', function () {
-    dhs.cleanTitle('\n                    kaiquewdev/docker-nodejs-co...\n                    \n                    \n                        \n                            4 months, 3 weeks ago\n                        ').should.be.eql('kaiquewdev/docker-nodejs-co...');
-  });
-
   it('should be return an empty list if has no keyword', function (done) {
     function searchHandler(err, result) {
       err.should.be.eql('Keyword should be an value.'); 
@@ -18,10 +14,9 @@ describe('Dhs', function() {
   it('should be return an result', function (done) {
     function searchHandler(err, result) {
       should.not.exist(err); 
-      result.should.have.lengthOf(1);
-      result[0].title.should.be.eql('kaiquewdev/docker-nodejs-co...');
+      result.should.have.lengthOf(10);
       done();
     }
-    dhs.search('docker-nodejs-compiled', searchHandler); 
+    dhs.search('ubu', searchHandler); 
   });
 });
